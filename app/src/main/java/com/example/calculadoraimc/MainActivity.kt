c74a26c6506ba49e405258c4a67eb9ef81673948
YO <ojara@gmx.com>
2024-06-05 09:41:47 +0200

    Primeros pasos con Android

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //AppCompatActivity es la HERENCIA
+
+    lateinit var wellcomeTextView: TextView
+    lateinit var Botondelclick: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //Vista de ventana App
+
+        wellcomeTextView = findViewById(R.id.wellcomeTextView)
+        Botondelclick = findViewById(R.id.Botondelclick)
+
+        wellcomeTextView.text = "Bienvenidos a mi primera App"
+
+        Botondelclick.setOnClickListener {
+            wellcomeTextView.text = "Realicé mi primer botón"
+        }
     }
 }
